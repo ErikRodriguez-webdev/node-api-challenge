1e9aec576ec405380dc642bc15869975d1f18611
Erik Rodriguez <eric19.21.03@gmail.com>
2020-03-13 08:36:21 -0700

    Add dependencies and basic files for server
    *express, cors dependencies
    *nodemon dev dependency
    *index.js has server.listen and port
    *server.js is housing all routers and helmet to protect headers

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+
+const server = express();
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "SERVER IS UP" });
+});
+
+//Routers
+
+module.exports = server;
